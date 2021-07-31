5a02a1a79d1d0ba6994bf93486adfcaeceb496fd
Ryan Fransen-Pruden <65836248+ryanfransenpruden@users.noreply.github.com>
2021-07-31 00:14:27 -0500

    Update stockprice.py

--- streamlit/stockprice.py
+++ streamlit/stockprice.py
@@ -1,20 +1,20 @@
- 	import yfinance as yf
-	import streamlit as st
+import yfinance as yf
+import streamlit as st
 	
-	st.write("""
-	# Simple Stock Price App
+st.write("""
+# Simple Stock Price App
 	
-	Shown are the stock closing price and volume!
+Shown are the stock closing price and volume!
 	
-	""")
+""")
 	
-	#define the ticker symbol
-	tickerSymbol = 'APPL'
-	#get data on this ticker
-	tickerData = yf.Ticker(tickerSymbol)
-	#get the historical prices for this ticker
-	tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
-	# Open	High	Low	Close	Volume	Dividends	Stock Splits
+#define the ticker symbol
+tickerSymbol = 'APPL'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
 	
-	st.line_chart(tickerDf.Close)
-	st.line_chart(tickerDf.Volume)
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
